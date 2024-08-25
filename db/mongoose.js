3916706939ec39bc928b1a71fc1d1f09dcc6eba3
Rajni <balatamna16@gmail.com>
2024-08-25 13:17:58 -0400

    mongoose.js file updated

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,9 +1,24 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const path = require("path");
+const { error } = require("console");
 
 const envFilePath = path.resolve(__dirname,"../../.env");
 
 dotenv.config({path:envFilePath})
 
-c
+const dbProtocol = process.env.DB_PROTOCOL
+const dbHost = process.env.DB_HOST
+const dbPort = process.env.DB_PORT
+const dbName = process.env.DB_NAME
+
+const dbConnectionString = `${dbProtocol}://${dbHost}:${dbPort}/${dbName}`;
+
+mongoose.connect(dbConnectionString,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(()=>{
+    console.log("Connected to database")
+}).catch((error)=>{
+    console.log("Error connecting to the database:",error)
+})
