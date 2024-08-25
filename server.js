3468abc5bee25fcb3f5a364c07e65e818c3af3d9
Rajni <balatamna16@gmail.com>
2024-08-25 11:29:18 -0400

    server.js updated

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 const express = require("express");
 //create instance of express app
-const app = 
+const app = express();
+//define port 
+const port = process.env.PORT || 8080;
+
+//use router in your app
+const router = require("./middleware/router");
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
